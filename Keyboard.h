f718a77d2d61b929736864ae03456d779cc3befa
h30e25 <87349782+h30e25@users.noreply.github.com>
2021-07-21 13:03:12 +0900

    プレイ画面遷移と操作説明遷移

---
+++ Keyboard.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// キーの入力状態を更新する
+void Keyboard_Update();
+
+// 引数のキーコードのキーの入力状態を返す
+int Keyboard_Get(int KeyCode);
---
+++ Menu.h
@@ -0,0 +1,7 @@
+#pragma once
+
+//XV
+void Menu_Update();
+
+//•`‰æ
+void Menu_Draw();
